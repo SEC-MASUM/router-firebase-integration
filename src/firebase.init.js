ee12f0bd56560373660949560a8b2a351282b59c
SEC-MASUM <masum.jess42@gmail.com>
2022-04-09 15:44:02 +0600

    do initial setup

---
+++ src/firebase.init.js
@@ -0,0 +1,15 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAxYneRermHNuT-DPPw4Rbqc2RN3p86POk",
+  authDomain: "router-firebase-integration-01.firebaseapp.com",
+  projectId: "router-firebase-integration-01",
+  storageBucket: "router-firebase-integration-01.appspot.com",
+  messagingSenderId: "903360043579",
+  appId: "1:903360043579:web:d7969d34112590a5f95a1e",
+};
+
+// Initialize Firebase
+const firebaseApp = initializeApp(firebaseConfig);
+
+export default firebaseApp;
