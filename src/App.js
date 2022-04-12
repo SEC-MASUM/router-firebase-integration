e46406c6cd1ede1986928a02e7078272052e91ff
SEC-MASUM <masum.jess42@gmail.com>
2022-04-12 06:29:40 +0600

    create Orders component and set private route for /orders

--- src/App.js
+++ src/App.js
@@ -3,8 +3,10 @@ import "./App.css";
 import Header from "./components/Header/Header";
 import Home from "./components/Home/Home";
 import Login from "./components/Login/Login";
+import Orders from "./components/Orders/Orders";
 import Products from "./components/Products/Products";
 import Register from "./components/Register/Register";
+import RequireAuth from "./components/RequireAuth/RequireAuth";
 
 function App() {
   return (
@@ -13,6 +15,14 @@ function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/products" element={<Products />} />
+        <Route
+          path="/orders"
+          element={
+            <RequireAuth>
+              <Orders />
+            </RequireAuth>
+          }
+        />
         <Route path="/register" element={<Register />} />
         <Route path="/login" element={<Login />} />
       </Routes>
